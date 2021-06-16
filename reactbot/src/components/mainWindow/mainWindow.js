06a36dcb314c0b57a80e8de60a751dc86292c21a
EG <tru.elli.yes@gmail.com>
2021-06-16 18:14:57 +0300

    try tu push tuegor some shit

--- reactbot/src/components/mainWindow/mainWindow.js
+++ reactbot/src/components/mainWindow/mainWindow.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import '../mainWindow/window.css';
 import '../mainWindow/heisenberg.svg';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import News from './news';
 import Outlines from './Outlines';
 import Rate from './rate';
 import RegistrPage from './registPage';
 import Tests from './testing';
+import { useRoutes } from '../routes';
 
 function MainWindow(){
+    const routes = useRoutes(0)
     return(
         <main id='promo' className = 'promo'>
-            <Switch>
+            
+                {routes}
+            
+            
+            {/* <Switch>
                 <Route exact path ='/news' component = {News}></Route>
                 <Route path ='/registPage' component = {RegistrPage}></Route>
                 <Route path ='/rate' component = {Rate}></Route>
                 <Route path ='/Outlines' component = {Outlines}></Route>
                 <Route path ='/testing' component = {Tests}></Route>                
-            </Switch>
+            </Switch> */}
 
         </main>
     )
---
+++ reactbot/src/components/mainWindow/registPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../mainWindow/window.css'
+
+
+export default function Outlines(){
+    return(
+        <div className = 'leftBlocks'>
+            <h1 className='h1'>Пожалуйста авторизуйтесь</h1>
+            <div>
+        
+            </div>
+            <div>
+                <div>Регистрация</div>
+                <div>Войти</div>
+            </div>
+        </div>
+    )
+}
