a59cfea11c358fe3a898795320a748218bc07333
EG <tru.elli.yes@gmail.com>
2021-06-19 16:57:49 +0300

    ww

--- reactbot/src/components/mainWindow/Outlines.js
+++ reactbot/src/components/mainWindow/Outlines.js
@@ -1,16 +1,52 @@
+import React from "react";
+import "../mainWindow/window.css";
 
-import React from 'react';
-import '../mainWindow/window.css';
+const styles = {
+  header: { marginLeft: "30px" },
+};
 
-export default function Outlines(){
-    return(
-        <div className = 'leftBlocks'>
-            <h1 className='h1'>Конспекты</h1>
-            <ul className='h1'>
-                <li>1тема</li>
-                <li>2тема</li>
-                <li>3тема</li>
-            </ul>
-        </div>
-    )
+export default function Outlines() {
+  return (
+    <div className="leftBlocks">
+      <div className="outlinesHeader">
+        <h1 className="h1" style={styles.header}>
+          Конспекты
+        </h1>
+      </div>
+      <div className="outlines">
+        <ul className="ul">
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+          <li>1тема</li>
+          <li>2тема</li>
+          <li>3тема</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
