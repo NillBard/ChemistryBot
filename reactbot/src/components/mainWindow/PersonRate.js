61422998c7fd12efeefb424617c14ca690ca851f
EG <tru.elli.yes@gmail.com>
2021-06-21 16:05:18 +0300

    egor

--- reactbot/src/components/mainWindow/PersonRate.js
+++ reactbot/src/components/mainWindow/PersonRate.js
@@ -1,20 +1,49 @@
-import React from 'react'
-import Account from '../menu/acc.svg';
-import '../mainWindow/window.css';
+import React from "react";
+import Account from "../menu/acc.svg";
+import "../mainWindow/window.css";
 
 const styles = {
-    text:{
-        display: 'flex', 
-    }
-}
+  text: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#4f688c",
+    marginTop: "10px",
+  },
+  img: { width: "40px", height: "40px" },
+  block: {
+    // backgroundColor: "red",
+    // width: "40px",
+    // height: "40px",
+    margin: "5px",
+    flexBasis: "40%",
+    color: "var(--primary-color)",
+  },
+  blockSecond: {
+    // width: "40px",
+    // height: "40px",
+    margin: "5px",
+    flexBasis: "10%",
+    color: "var(--primary-color)",
+  },
+};
 
- const PersonRate = (props) =>{
-    return(
-        <div style={styles.text} >
-            <img src= {props.avatar}/>
-            <span>{props.name}</span>
-        </div>
-    )
-}
+const PersonRate = (props) => {
+  return (
+    <div style={styles.text}>
+      <div style={styles.blockSecond}>
+        <span>1</span>
+      </div>
+      <div style={styles.blockSecond}>
+        <img src={props.avatar} style={styles.img} />
+      </div>
+      <div style={styles.block}>
+        <span>{props.name}</span>
+      </div>
+      <div style={styles.block}>
+        <span>10420</span>
+      </div>
+    </div>
+  );
+};
 
 export default PersonRate;
--- reactbot/src/components/mainWindow/rate.js
+++ reactbot/src/components/mainWindow/rate.js
@@ -1,37 +1,48 @@
+import React from "react";
+import "../mainWindow/window.css";
+import Champion from "../mainWindow/Champion";
+import PersonRate from "../mainWindow/PersonRate";
+import Ac from "../mainWindow/heisenberg.svg";
 
-import React from 'react';
-import '../mainWindow/window.css';
-import Champion from '../mainWindow/Champion'
-import PersonRate from '../mainWindow/PersonRate'
-import Ac from '../mainWindow/heisenberg.svg';
+const styles = {
+  header: { marginLeft: "30px" },
+};
 
+function Rate() {
+  const Account = {
+    name: "Андрей",
+    avatar: Ac,
+  };
 
-
-
- function Rate(){
-    const Account = {
-        name: 'Андрей',
-        avatar: Ac 
-    }
-    return(
-        
-        <div className = 'RateBlock' >
-            <div className ='headRate'>
-                <h1 className='h1'>Рейтинг</h1>
-            </div>
-            <div className='champions'>
-                <div className='champions first'><Champion /></div>
-                <div className='champions second'><Champion/><Champion/></div>
-                <div>
-                    <ul>
-                        <PersonRate name = {Account.name} avatar = {Account.avatar}></PersonRate>
-  
-                        
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="leftBlocks">
+      <div className="outlinesHeader">
+        <h1 className="h1" style={styles.header}>
+          Рейтинг
+        </h1>
+      </div>
+      <div className="champions">
+        <div className="champions first">
+          <Champion />
         </div>
-    )
+        <div className="champions second">
+          <Champion />
+          <Champion />
+        </div>
+        <div>
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+          <PersonRate name={Account.name} avatar={Account.avatar} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rate;
