3a25a6923790819e274fda76a3f79a1a7ecd8d98
EG <tru.elli.yes@gmail.com>
2021-06-22 07:42:13 +0300

    egor

--- reactbot/src/components/mainWindow/testCard.js
+++ reactbot/src/components/mainWindow/testCard.js
@@ -1,6 +1,33 @@
 import React from "react";
+import question from "../../tests/test/task1_11.png";
 
-export default function TestCard({ link }) {
-  console.log(link);
-  return <h1>{link}</h1>;
+const styles = {
+  but: {
+    width: "200px",
+    height: "50px",
+    margin: "0",
+  },
+  row: {
+    display: "flex",
+    justifyContent: "space-around",
+    marginTop: "20px",
+  },
+};
+
+export default function TestCard(props) {
+  const id = props.links;
+  return (
+    <div>
+      <img src={question} />
+      <div style={styles.row}>
+        <span className="h1">Введите ответ:</span>
+      </div>
+      <div style={styles.row}>
+        <input />
+        <div className="button login" style={styles.but}>
+          OK
+        </div>
+      </div>
+    </div>
+  );
 }
