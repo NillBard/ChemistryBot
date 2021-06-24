efbe525266fe7c74dce093723ffb75e5991acde0
EG <tru.elli.yes@gmail.com>
2021-06-24 03:12:23 +0300

    Added account page lgout

--- reactbot/src/components/mainWindow/AccInfo.js
+++ reactbot/src/components/mainWindow/AccInfo.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./window.css";
 import avatar from "./heisenberg.svg";
+import { AuthContext } from "../../redux/authContext";
+import { useHistory } from "react-router-dom";
 
 const styles = {
   header: { marginLeft: "30px" },
@@ -14,9 +16,26 @@ const styles = {
     // alignItems: "center",
   },
   tdi: { backgroundColor: "#4f688c", color: "red" },
+  alert: {
+    backgroundColor: "red",
+    marginTop: "0px",
+    width: "200px",
+    height: "40px",
+  },
+  a: {
+    textDecoration: "none",
+  },
 };
 
 export default function AccInfo() {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const logOutHandler = (event) => {
+    event.preventDefault();
+    auth.logout();
+    history.push("/");
+  };
+
   return (
     <div className="leftBlocks">
       <div className="outlinesHeader">
@@ -69,6 +88,13 @@ export default function AccInfo() {
                 </td>
               </tr>
             </table>
+          </div>
+        </div>
+        <div className="accInfoInfo">
+          <div className="button login" style={styles.alert}>
+            <a href={"/"} style={styles.a} onClick={logOutHandler}>
+              <span>Выйти</span>
+            </a>
           </div>
         </div>
       </div>
